bb991b0a3b26b83455a7fda0f92c2cc52ca51129
22-04-80 <s220480@student.uek.krakow.pl>
2022-01-31 18:22:26 +0100

    Add inline web worker spawner

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@ const second = document.querySelector('#number2');
 
 const result = document.querySelector('.result');
 
+const workers = [];
+
 if (window.Worker) {
 	const myWorker = new Worker("scripts/worker.js");
 
@@ -12,6 +14,28 @@ if (window.Worker) {
 
 	function triggerErrorInWorker() {
 		myWorker.postMessage({type:"err", data: {}})
+	}
+
+	function createCustomWorker(task) {
+		const blob = new Blob([`onmessage=${task.toString()}`])
+		return  new Worker(window.URL.createObjectURL(blob));
+	}
+
+	function spawnWorker() {
+		let newWorker = createCustomWorker(function (messageEvent) {
+			console.log("hello")
+			let counter = 0;
+			setInterval(function () {
+				postMessage({type: "counter", data: {counter: counter++}})
+			}, 1000);
+		})
+		workers.push(newWorker)
+		newWorker.postMessage("hi")
+		newWorker.onmessage = function(e) {
+			console.log("Main thread received message", e.data)
+			result.textContent = JSON.stringify(e.data);
+			console.log('Message received from worker');
+		}
 	}
 
 	first.onchange = function() {
