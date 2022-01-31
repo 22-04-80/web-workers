41241d1562f9db91e170a9b8b7e5aa5ac7a11d14
22-04-80 <s220480@student.uek.krakow.pl>
2022-01-31 17:45:32 +0100

    Add several more messages

--- scripts/main.js
+++ scripts/main.js
@@ -6,18 +6,27 @@ const result = document.querySelector('.result');
 if (window.Worker) {
 	const myWorker = new Worker("scripts/worker.js");
 
+	function sendComplexMessage() {
+		myWorker.postMessage({type:"msg", data: {num: 1}})
+	}
+
+	function triggerErrorInWorker() {
+		myWorker.postMessage({type:"err", data: {}})
+	}
+
 	first.onchange = function() {
-		myWorker.postMessage([first.value, second.value]);
+		myWorker.postMessage({type: "calc", data: [first.value, second.value]});
 		console.log('Message posted to worker');
 	}
 
 	second.onchange = function() {
-		myWorker.postMessage([first.value, second.value]);
+		myWorker.postMessage({type: "calc", data: [first.value, second.value]});
 		console.log('Message posted to worker');
 	}
 
 	myWorker.onmessage = function(e) {
-		result.textContent = e.data;
+		console.log("Main thread received message", e.data)
+		result.textContent = JSON.stringify(e.data);
 		console.log('Message received from worker');
 	}
 } else {
--- scripts/worker.js
+++ scripts/worker.js
@@ -1,11 +1,31 @@
 onmessage = function(e) {
-	console.log('Worker: Message received from main script');
-	const result = e.data[0] * e.data[1];
-	if (isNaN(result)) {
-		postMessage('Please write two numbers');
-	} else {
-		const workerResult = 'Result: ' + result;
-		console.log('Worker: Posting message back to main script');
-		postMessage(workerResult);
+	switch (e.data.type) {
+		case "calc": {
+			const result = e.data.data[0] * e.data.data[1];
+			if (isNaN(result)) {
+				postMessage('Please write two numbers');
+			} else {
+				const workerResult = 'Result: ' + result;
+				console.log('Worker: Posting message back to main script');
+				postMessage(workerResult);
+			}
+			break;
+		}
+		case "msg": {
+			console.log("worker received complex message:", e.data.type, e.data.data)
+			break;
+		}
+		case "err": {
+			throw new Error("Triggered error")
+		}
+		default: {
+			console.log("worker received message with data:", e.data)
+		}
 	}
+	console.log('Worker: Message received from main script', {data: e.data});
+}
+
+onerror = function (error) {
+	console.log('!!!', error)
+	postMessage({type: "error", error})
 }
